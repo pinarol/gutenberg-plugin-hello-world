ae9436dd56ebf4e47c3669b7d955c30ab09a88a8
Matthew Kevins <mmkevins@yahoo.com>
2019-06-21 23:23:46 +1000

    Import React

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import React from 'react';
 import { registerBlockType } from '@wordpress/blocks';
 import { RichText } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
