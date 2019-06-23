e26b9dccc82e2fd999d710408758f7edd2e1bdcf
Matthew Kevins <mmkevins@yahoo.com>
2019-06-23 22:04:28 +1000

    Add webpack config to bundle minified build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  optimization: {
+    //minimizer: []
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  externals: [
+    {
+      react: 'require(\'react\')'
+    },
+    {
+      "@wordpress/blocks": "require('@wordpress/blocks')"
+    },
+    {
+      "@wordpress/editor": "require('@wordpress/editor')"
+    },
+    {
+      "@wordpress/i18n": "require('@wordpress/i18n')"
+    },
+  ]
+};
